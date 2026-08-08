c27d47ce0c53b6418628f3c3e03b3d87b717e38f
DamThanQuy <qdam100@gmail.com>
2026-08-09 03:15:15 +0700

    fix(web): restore dark theme via ThemeProvider + fix alembic multiple heads (bookings migration)

--- apps/api/migrations/versions/book0000000001_add_mock_bookings.py
+++ apps/api/migrations/versions/book0000000001_add_mock_bookings.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'book0000000001'
-down_revision: Union[str, None] = 'rag0000000006'
+down_revision: Union[str, None] = 'rag0000000008'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
